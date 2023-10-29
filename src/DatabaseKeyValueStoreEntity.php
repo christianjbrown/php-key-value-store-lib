e1ed7e0e9f2850f7183f44cfcdcfbdb635b19d2e
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-10-29 14:09:28 +0000

    Add the DatabaseKeyValueStore backend and its entity

---
+++ src/DatabaseKeyValueStoreEntity.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChristianBrown\KeyValueStore;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class DatabaseKeyValueStoreEntity
+{
+    #[ORM\Id]
+    #[ORM\Column(type: Types::STRING, nullable: false)]
+    private ?string $key = null;
+
+    #[ORM\Column(type: Types::INTEGER, nullable: true)]
+    private ?string $ttl = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $value = null;
+
+    final public function getKey(): ?string
+    {
+        return $this->key;
+    }
+
+    final public function getTtl(): ?int
+    {
+        return $this->ttl;
+    }
+
+    final public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    final public function setKey(?string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    final public function setTtl(?int $ttl): self
+    {
+        $this->ttl = $ttl;
+
+        return $this;
+    }
+
+    final public function setValue(?string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
