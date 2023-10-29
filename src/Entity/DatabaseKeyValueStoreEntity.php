936e95052a0738dff454e6b3be316db63864b0dd
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-10-29 14:44:13 +0000

    Updates

--- src/Entity/DatabaseKeyValueStoreEntity.php
+++ src/Entity/DatabaseKeyValueStoreEntity.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace ChristianBrown\KeyValueStore;
+namespace ChristianBrown\KeyValueStore\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
